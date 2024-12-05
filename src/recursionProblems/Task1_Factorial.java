982ef3d1f665670a8df339f88ea4626a98321f85
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-12-05 10:30:59 +0200

    solving Task1_Factorial from Recursion Coding Tasks 1

---
+++ src/recursionProblems/Task1_Factorial.java
@@ -0,0 +1,21 @@
+package recursionProblems;
+
+import java.util.Scanner;
+
+public class Task1_Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.println(Factorial(n));
+
+    }
+
+    public static int Factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        return n * Factorial(n - 1);
+    }
+}
