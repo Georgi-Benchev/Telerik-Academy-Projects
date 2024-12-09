b853f6b0c731b67ab33b041d4ce75561aa835f1e
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-12-09 11:53:34 +0200

    solving Task4_BunnyEars2 from Recursion Coding Tasks 1

---
+++ src/recursionProblems/CodingTasks1/Task4_BunnyEars2.java
@@ -0,0 +1,22 @@
+package recursionProblems.CodingTasks1;
+
+import java.util.Scanner;
+
+public class Task4_BunnyEars2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfBunnies = Integer.parseInt(scanner.nextLine());
+        System.out.println(bunnyEarsCounter(numberOfBunnies));
+    }
+
+    public static int bunnyEarsCounter(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return 3 + bunnyEarsCounter(n - 1);
+        }
+        return 2 + bunnyEarsCounter(n - 1);
+    }
+}
