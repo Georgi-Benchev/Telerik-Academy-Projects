4c46af2cf0d752702bf70c7434fd0ff731e7921b
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-12-09 11:54:49 +0200

    solving Task11_CountHi from Recursion Coding Tasks 1

---
+++ src/recursionProblems/CodingTasks1/Task11_CountHi.java
@@ -0,0 +1,29 @@
+package recursionProblems.CodingTasks1;
+
+import java.util.Scanner;
+
+public class Task11_CountHi {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        System.out.println(CountHi(input));
+    }
+
+    public static int CountHi(String input) {
+
+        if (input.length() > 2) {
+            if ("hi".equals(input.substring(0, 2))) {
+                return 1 + CountHi(input.substring(1));
+            }
+            return CountHi(input.substring(1));
+        } else {
+            if (input.equals("hi")) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
